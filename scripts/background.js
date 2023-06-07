5cebb02483d51df499200cb750054047b7b539fa
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-06-07 23:11:35 +0200

    Fix background script Now backgrount script takes from 20% to 150% les ram. It's more stable and don't fetch all content at once

--- scripts/background.js
+++ scripts/background.js
@@ -1,57 +1,80 @@
 const links = [
-    'https://raw.githubusercontent.com/Azalurg/homepage/main/script/index.js', 
+    'https://raw.githubusercontent.com/Azalurg/homepage/main/script/index.js',
     'https://raw.githubusercontent.com/Azalurg/homepage/main/script/weather.js',
-    'https://raw.githubusercontent.com/Azalurg/homepage/main/script/bookmarks.js',
     'https://raw.githubusercontent.com/Azalurg/Crypta/master/src/tools/vigenereBeispiel.py',
     'https://raw.githubusercontent.com/Azalurg/yt-mp3/main/script.py',
     'https://raw.githubusercontent.com/Azalurg-Uni-Projects/003-java-freeMarket/main/Market/game/Game.java'
 ];
 
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+let word_speed = 100;
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-  
     return array;
-  }
+};
 
-const shuffledLinks = shuffle(links)
+function get_text(link) {
+    return fetch(link).then(response => response.text());
+};
 
-// randomly select delay
-const delay = Math.floor(Math.random() * 100)+10;
+function max(a, b) {
+    if (a > b) return a;
+    return b;
+};
 
-// fetch the links and read the text
-function print(link, counter, power) {
-    fetch(shuffledLinks[link])
+function delay(char) {
+    if (char == ' '){
+        word_speed = Math.floor(Math.random() * 50) + 10;
+        return 150;
+    } 
+    word_speed += 1;
+    return max(word_speed, 100);
+};
+
+function print(text, index, callback) {
+    let char = text[index];
+    if (text[index] == '\n') char = '<br>';
+
+    setTimeout(() => {
+        document.querySelector('#background').innerHTML += char;
+        if (index < text.length - 1) {
+            print(text, index + 1, callback);
+        } else {
+            document.querySelector('#background').innerHTML += '<br><br>';
+            callback();
+        }
+    }, delay(char));
+};
+
+function fetch_and_print() {
+    const link = shuffleArray(links).pop();
+    if (!link) {
+        console.log("finish");
+        return;
+    }
+
+    fetch(link)
         .then(response => response.text())
         .then(text => {
-            // read text char by char and pass to html with 0.5s delay for a char
-            let i = 0
-            for (i; i < text.length; i++) {
-                let char = text[i];
-                if (text[i] == '\n') char = '<br>';
-
-                setTimeout(() => {
-                    document.querySelector('#background').innerHTML += char;
-                }, delay * (i+counter) * power);
-            }
-            return i
+            print(text, 0, fetch_and_print);
         })
-        .then((i) =>{
-            if(link > 0){
-                print(link - 1, power == 0 ? 1 : i+counter, 1)
-            }
-        })
-}
+        .catch(error => console.error(`Error fetching ${link}:`, error))
+};
 
-print(shuffledLinks.length-1, 0, 0)
+fetch(shuffleArray(links).pop())
+    .then(response => response.text())
+    .then(text => {
+        let inner_HTML = '';
+        for (let i = 0; i < text.length; i++) {
+            let char = text[i];
+            if (text[i] == '\n') char = '<br>';
+            inner_HTML += char;
+        }
+        document.querySelector('#background').innerHTML += inner_HTML;
+        document.querySelector('#background').innerHTML += '<br><br>';
+        fetch_and_print();
+    })
+    .catch(error => console.error(`Error fetching ${link}:`, error));
